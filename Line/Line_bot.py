dfdb87e068374308ef4e16bfab595dc292c73eb9
yanyi-hsu <vincent91112911@gmail.com>
2024-11-04 13:39:20 +0800

    改為一上線就開始偵測

--- Line/Line_bot.py
+++ Line/Line_bot.py
@@ -9,26 +9,26 @@ from sklearn.preprocessing import LabelEncoder
 from ultralytics import YOLO
 from datetime import datetime
 from torch import nn
-import numpy as np
 import torch
 import json
 import cv2
-import time
+import threading
 
 with open('Line/setting.json', 'r', encoding='UTF8') as file:
     jfile = json.load(file)
 
 with open('Line/user.json', 'r', encoding='UTF8') as file:
     juser = json.load(file)
+    
 
 app = Flask(__name__) 
 
 port = "5000"
 
-line_bot_api = LineBotApi(jfile['token'])              # 確認 token 是否正確
-handler = WebhookHandler(jfile['secret'])              # 確認 secret 是否正確
+line_bot_api = LineBotApi(jfile['token'])
+handler = WebhookHandler(jfile['secret'])
 
-user_dict = {}
+message_sent = False
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size=34, hidden_size1=64, hidden_size2=32, num_classes=2):
@@ -62,10 +62,27 @@ encoder.fit(['fall', 'not fallen'])
 
 @app.route("/", methods=['POST'])
 def linebot():
-    signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
-    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    
+    global message_sent
+    if not message_sent:
+        user_id = juser["user_id"]["user_id_許晏益"]
+        online_message = f">> 服務已上線 <<\n已開始偵測"
+        
+        '''
+        for user_id in juser['user_id'].values():
+            line_bot_api.push_message(user_id, TextSendMessage(text=online_message))
+            line_bot_api.push_message(user_id, StickerSendMessage(package_id=11538, sticker_id=51626530))
+        ''' 
+
+        line_bot_api.push_message(user_id, TextSendMessage(text=online_message))
+        line_bot_api.push_message(user_id, StickerSendMessage(package_id=11538, sticker_id=51626530))
+        message_sent = True
+        #threading.Thread(target=Detect_Fall).start()
+           
     try:
-        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'    
@@ -75,83 +92,66 @@ def handle_follow(event):
     user_id = event.source.user_id
     profile = line_bot_api.get_profile(user_id)
     display_name =profile.display_name
-    # 儲存 user_id 或做其他處理
-    print(f"新加入的使用者ID: {user_id}")
-    user_dict['user_id'][f'user_id_{display_name}'] = user_id
-    with open('Line/user.json', 'w', encoding='utf8') as file:
-        json.dump(user_dict, file, indent=4, ensure_ascii=False)
 
-    # 回應訊息給使用者
+    print(f"新加入的使用者ID: {display_name} : {user_id}")
+    juser['user_id'][f'user_id_{display_name}'] = user_id
+    with open('Line/user.json', 'w', encoding='utf8') as file:
+        json.dump(juser, file, indent=4, ensure_ascii=False)
+
     welcome_message = f"感謝{display_name}加我為好友！！！"
-    message = "打出 >> 開始偵測 <<，即開始偵測。"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=welcome_message))
-    line_bot_api.push_message(user_id, TextSendMessage(text=message))
     line_bot_api.push_message(user_id, StickerSendMessage(package_id=11538, sticker_id=51626494))
 
 @handler.add(MessageEvent, message=TextMessage)
-def Detect_Fall(event):
+def handle_message(event):
     user_id = event.source.user_id
     profile = line_bot_api.get_profile(user_id)
-    display_name = profile.display_name
-    if user_id in juser["user_id"].values():
-        pass
-    else:
-        user_dict['user_id'][f'user_id_{display_name}'] = user_id
-        with open('Line/user.json', 'w', encoding='utf8') as file:
-            json.dump(user_dict, file, indent=4, ensure_ascii=False)
-    
-    if event.message.text == "開始偵測":
-        message = "已開始偵測"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
-        monitor = cv2.VideoCapture('rtsp://Program:000000@192.168.0.102/stream1')
-        last_detect:datetime = None
-        delta = datetime.now() - datetime.now()
-        try:
-            while monitor.isOpened():
-                ret, frame = monitor.read()
-                work_time = datetime.now()
-                if ret:
-                    results = pose_model.predict(frame, verbose=False)
+    display_name =profile.display_name
+    message = f"{display_name}\n謝謝你願意找我聊天\n但我不會聊天喔！"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+    line_bot_api.push_message(user_id, StickerSendMessage(package_id=11539, sticker_id=52114110))
+    juser['user_id'][f'user_id_{display_name}'] = user_id
+    with open('Line/user.json', 'w', encoding='utf8') as file:
+        json.dump(juser, file, indent=4, ensure_ascii=False)
+'''
+def Detect_Fall():
+    monitor = cv2.VideoCapture('rtsp://Program:000000@192.168.0.102/stream1')
+    last_detect:datetime = None
+    delta = datetime.now() - datetime.now()
+    user_id = juser["user_id"]["user_id_許晏益"]
+    while monitor.isOpened():
+        ret, frame = monitor.read()
+        if ret:
+            results = pose_model.predict(frame, verbose=False)
 
-                    if(last_detect == None):
-                        pass
-                    else:
-                        delta = datetime.now() - last_detect
+            if(last_detect == None):
+                pass
+            else:
+                delta = datetime.now() - last_detect
 
-                    for result in results:
-                        keypoints = result.keypoints.xy.cpu().numpy().flatten()
-                        if keypoints.size == 34:
-                            input_tensor = torch.from_numpy(keypoints).to(device)
-                            with torch.no_grad():
-                                out = model_inference(input_tensor)
-                                if out.dim() == 1:
-                                    out = out.unsqueeze(0)
-                                _, predicted_class = torch.max(out, 1)
+            for result in results:
+                keypoints = result.keypoints.xy.cpu().numpy().flatten()
+                if keypoints.size == 34:
+                    input_tensor = torch.from_numpy(keypoints).to(device)
+                    with torch.no_grad():
+                        out = model_inference(input_tensor)
+                        if out.dim() == 1:
+                            out = out.unsqueeze(0)
+                        _, predicted_class = torch.max(out, 1)
 
-                            predicted_label = encoder.inverse_transform(predicted_class.cpu().numpy())
+                    predicted_label = encoder.inverse_transform(predicted_class.cpu().numpy())
 
-                            delta = datetime.now() - last_detect if last_detect else datetime.now() - datetime.now()
+                    delta = datetime.now() - last_detect if last_detect else datetime.now() - datetime.now()
 
-                            #for (x, y) in keypoints.reshape(-1, 2):
-                                #cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
-                            #cv2.putText(frame, f'Predicted: {predicted_label[0]}', (900, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-                            if 'fall' in predicted_label and (delta.seconds > 30 or last_detect == None):
-                                print(f'last: {last_detect}')
-                                print(f'delta:{delta}')
-                                line_bot_api.push_message(user_id, TextSendMessage(text=f"有人跌倒了！\n時間：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
-                                line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=jfile['picture'], preview_image_url=jfile['picture']))
-                                last_detect = datetime.now()
-                            
-                    #cv2.imshow('Yolov8m-pose with Classification', frame) 
-                else:
-                    print('>> Monitor Error <<')
-                    break
-        except Exception as e:
-            print(e)
-        finally:           
-            monitor.release()
-            cv2.destroyAllWindows()
+                    if 'fall' in predicted_label and (delta.seconds > 30 or last_detect == None):
+                        print(f'last: {last_detect}')
+                        print(f'delta:{delta}')
+                        line_bot_api.push_message(user_id, TextSendMessage(text=f"有人跌倒了！\n時間：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
+                        line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=jfile['picture'], preview_image_url=jfile['picture']))
+                        last_detect = datetime.now()       
+        else:
+            pass
+'''
 
 if __name__ == "__main__":
     public_url = ngrok.connect(port).public_url
